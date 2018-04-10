1f09454893b32742f89f26628d606ee9fcd9b9d2
rogersonpi <iain.rogerson77@googlemail.com>
2018-04-10 16:22:31 +0100

    updated files

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,25 @@
+def return_10()
+
+  return 10
+end
+
+def add(num_one, num_two)
+  return num_one + num_two
+end
+
+def subtract(num_one, num_two)
+  return num_one - num_two
+end
+
+def multiply (num_one, num_two)
+  return num_one * num_two
+end
+
+def divide (num_one, num_two)
+  return num_one / num_two
+end
+
+def length_of_string()
+   test_string = "A string of length 21"
+   test_string.length == 21
+ end
